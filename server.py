8b67066e1808d86367efe4841ca533910979f976
Tanishq-Banyal <banyaltanishq@gmail.com>
2022-03-13 17:32:36 +0000

    sqlite3, f strings and bug fixes

--- server.py
+++ server.py
@@ -1,70 +1,77 @@
-from bottle import route, run, post, request, template
-import mysql.connector
-import os
-file_names = []
+#!/usr/bin/python3
 
-users_db = mysql.connector.connect(
-    host="127.0.0.1",
-    user="root",
-    port="3306",
-    password="sql123",
-    database="users"
-)
+import os
+import sys
+import sqlite3 as sql
+from bottle import route, run, post, request, template
+
+if not os.path.exists('users.db'):
+	users_db = sql.connect('users.db')
+	users_cursor = users_db.cursor()
+	users_cursor.execute("CREATE TABLE users_table(uname varchar(50), fname varchar(50), lname varchar(50), mob varchar(10), pwd varchar(30));")
+	users_cursor.execute("CREATE TABLE items(uname varchar(50), item_name varchar(50), no_of_items int);")
+	users_db.commit()
+	users_db.close()
+
+users_db = sql.connect('users.db')
 users_cursor = users_db.cursor()
+
+file_names = []
 
 @route('/view')
 def view_page():
-    tpl_file = open("view.html")
+    tpl_file = open('view.html')
     tpl = tpl_file.read()
     tpl_file.close()
     return template(tpl, table=[["NO Data","NO Data" ], ["NO Data","NO Data" ]])
 
 @post('/add_user')
 def add_user():
-    uname = request.forms.get("uname")
-    fname = request.forms.get("fname")
-    lname = request.forms.get("lname")
-    mob = request.forms.get("mob")
-    pwd = request.forms.get("pwd")
-    users_cursor.execute("INSERT INTO users_table (uname, fname, lname, mob, pwd) VALUES (%s, %s, %s, %s, %s);", (uname, fname, lname, mob, pwd))
+    uname = request.forms.get('uname')
+    fname = request.forms.get('fname')
+    lname = request.forms.get('lname')
+    mob = request.forms.get('mob')
+    pwd = request.forms.get('pwd')
+    users_cursor.execute(f'INSERT INTO users_table VALUES ("{uname}", "{fname}", "{lname}", "{mob}", "{pwd}");')
     users_db.commit()
+    return 'Signup Success!'
 
 @post('/view')
 def view():
-    uname = request.forms.get("uname")
-    pwd = request.forms.get("pwd")
-    users_cursor.execute("SELECT pwd FROM users_table WHERE uname=\"%s\";" % (uname))
+    uname = request.forms.get('uname')
+    pwd = request.forms.get('pwd')
+    users_cursor.execute(f'SELECT pwd FROM users_table WHERE uname="{uname}";')
     correct_pwd = users_cursor.fetchall()[0][0]
     if pwd == correct_pwd:
-        users_cursor.execute("SELECT * FROM items WHERE uname=\"%s\";" % (uname))
-        tpl_file = open("view.html")
+        users_cursor.execute(f'SELECT * FROM items WHERE uname="{uname}";')
+        tpl_file = open('view.html')
         tpl = tpl_file.read()
         tpl_file.close()
         return template(tpl, table=users_cursor.fetchall())
     else:
-        return "Incorrect Password <a href=\"/\">Click here to go back</a>"
+        return 'Incorrect Password <a href="/">Click here to go back</a>'
 
 @route('/')
 def default_page():
     if not file_names:
         for f in os.listdir('.'):
             file_names.append(os.path.splitext(f)[0][0])
-    page_file = open("index.html")
+    page_file = open('index.html')
     page = page_file.read()
     page_file.close()
     return page
 
 @route('/<page>')
-def hello(page=""):
+def hello(page=''):
     if not file_names:
         for f in os.listdir('.'):
             file_names.append(os.path.splitext(f)[0])
     if page in file_names: 
-        page_file = open(page + ".html")
+        page_file = open(page + '.html')
         page = page_file.read()
         page_file.close()
         return page
     else:
-        return "This page does not exist yet, <a href=\"/\">Click here to go back</a>"
+        return 'This page does not exist yet, <a href="/">Click here to go back</a>'
 
 run(host='localhost', port=8080, debug=True)
